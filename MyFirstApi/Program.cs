9fa99c015f30bd814cc49590718ed1af2c946370
Ricardo Metzger <r7metzger@gmail.com>
2025-08-21 22:19:06 -0500

    feat: configurar ruta de bienvenida

--- MyFirstApi/Program.cs
+++ MyFirstApi/Program.cs
@@ -1,21 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.Use(async (context, next) =>
 {
-    app.MapOpenApi();
-}
-
-app.UseAuthorization();
-
-app.MapControllers();
-
+    if (context.Request.Path == "/") context.Response.Redirect("/index.html");
+    else await next();
+});
 app.Run();
